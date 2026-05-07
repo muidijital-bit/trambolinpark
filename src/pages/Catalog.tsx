b4c2a7bbb68ad0fa2eeedd325d9578824ec46a69
Mehmet <mehmet@192.168.1.34>
2026-05-07 11:13:34 +0300

    Fix unused import

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -1,4 +1,4 @@
-import { Link, useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import ProductCard from '../components/ui/ProductCard';
 import { allProducts } from '../data/mockData';
 
